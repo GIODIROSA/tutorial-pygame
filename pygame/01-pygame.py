33f2e70824e0b552555cc658ed0dc1e4a2ca1fbf
giodirosa <giodirosaartes@gmail.com>
2024-11-16 17:19:20 -0300

    [ADD] se agrega nombre de pantalla y while

--- pygame/01-pygame.py
+++ pygame/01-pygame.py
@@ -1,11 +1,10 @@
 import pygame
+import constantes
 
 pygame.init()
 
-width = 800
-height = 600
-
-ventana = pygame.display.set_mode((width, height))
+ventana = pygame.display.set_mode((constantes.WIDTH_WINDOWS, constantes.HEIGHT_WINDOWS))
+pygame.display.set_caption("Consumo electrico")
 
 run = True
 while run:
